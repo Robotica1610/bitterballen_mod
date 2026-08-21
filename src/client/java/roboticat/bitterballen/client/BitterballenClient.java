84c1064eef57a52c5f0f998a897ca502009f2663
Robotica1610 <JonathanSchilstra@gmail.com>
2026-08-21 16:19:54 +0200

    Initial commit

---
+++ src/client/java/roboticat/bitterballen/client/BitterballenClient.java
@@ -0,0 +1,10 @@
+package roboticat.bitterballen.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BitterballenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/roboticat/bitterballen/client/BitterballenDataGenerator.java
@@ -0,0 +1,11 @@
+package roboticat.bitterballen.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BitterballenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
