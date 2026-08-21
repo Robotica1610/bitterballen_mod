0db0e4ca85b51e972ddb6b9dfe7d9ff380c1e56e
Robotica1610 <JonathanSchilstra@gmail.com>
2026-08-21 17:17:22 +0200

    Added Bitterballen

--- src/main/java/roboticat/bitterballen/item/ModItems.java
+++ src/main/java/roboticat/bitterballen/item/ModItems.java
@@ -1,7 +1,49 @@
 package roboticat.bitterballen.item;
 
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import roboticat.bitterballen.Bitterballen;
+
+import java.util.function.Function;
+
 public class ModItems {
-    public static final
+
+    public static final Item BITTERBALLEN = registerItem("bitterballen", properties -> new Item(properties.food(new FoodProperties.Builder()
+            .nutrition(5)
+            .saturationModifier(3.0F)
+            .build())));
+
+    public static final Item RAWBITTERBALLEN = registerItem("raw_bitterballen", properties -> new Item(properties.food(new FoodProperties.Builder()
+            .nutrition(1)
+            .saturationModifier(0.0F)
+            .build())));
+
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+
+
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(Bitterballen.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Bitterballen.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(output -> {
+            output.accept(BITTERBALLEN);
+            output.accept(RAWBITTERBALLEN);
+        } );
+
+
+    }
+
 
 
 }
